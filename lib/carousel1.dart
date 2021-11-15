801a734a51ee1671b81af2e62bfc02887dfe9f8f
MrKhan8627 <se120182053uf@gmail.com>
2021-11-15 23:37:38 +0500

    add images

---
+++ lib/carousel1.dart
@@ -0,0 +1,42 @@
+// import 'package:flutter/material.dart';
+// import 'package:carousel_slider/carousel_slider.dart';
+//
+// class Carousel1 extends StatefulWidget {
+//   const Carousel1({Key? key}) : super(key: key);
+//
+//   @override
+//   _Carousel1State createState() => _Carousel1State();
+// }
+//
+// class _Carousel1State extends State<Carousel1> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       height: 182.0,
+//       child: CarouselSlider(
+//         boxFit: BoxFit.cover,
+//         dotColor: const Color(0xFF6991C7).withOpacity(0.8),
+//         dotSize: 5.5,
+//         dotSpacing: 16.0,
+//         dotBgColor: Colors.transparent,
+//         showIndicator: true,
+//         overlayShadow: true,
+//         overlayShadowColors: Colors.white.withOpacity(0.9),
+//         overlayShadowSize: 0.9,
+//         images: const [
+//           AssetImage("assets/img/baner1.png"),
+//           AssetImage("assets/img/baner12.png"),
+//           AssetImage("assets/img/baner2.png"),
+//           AssetImage("assets/img/baner3.png"),
+//           AssetImage("assets/img/baner4.png"),
+//         ], items: const [
+//         'assets/images/DeepLearning.png'
+//             'assets/images/DeepWorkPlan.png'
+//             'assets/images/ImageProcessing.png'
+//             'assets/images/MachineLearning.png'
+//             'assets/images/python.png'
+//       ],
+//       ),
+//     );
+//   }
+// }
